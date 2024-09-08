c13703f38f0394902b936346f9db47cd1692156c
Pahanpretty <pifpaf96@mail.ru>
2024-09-08 17:08:48 +0300

    first commit

---
+++ target/test-classes/test.sql
@@ -0,0 +1,5 @@
+insert into users (id, name, age, email) value (1,'Alex', 22, 'pifpaf96@mail.ru');
+#select * from users;
+#select * from users where name = 'Steve';
+#update users set name = 'Steve', age = 55, email = 'ankosnak@gmail.com';
+#delete from users where id = 5;
